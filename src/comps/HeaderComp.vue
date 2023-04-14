42ba59f93fbf159cf0a0613fd2de12a275da5829
kseniia <kseniia@smartiot.cloud>
2023-04-14 22:24:20 +0300

    add header component

---
+++ src/comps/HeaderComp.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <img src="/assets/icons/logo.png">
+    <span>SIGMA SCHOOL</span>
+  </div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style lang="scss" scoped>
+@import '../assets/variables.scss';
+
+div {
+  background-color: $secondary-bg;
+  padding: 10px 60px;
+  display: flex;
+  align-items: center;
+  gap: 8px;
+  box-shadow: 0px 3px 15px rgba(0, 0, 0, 0.1);
+
+  span {
+    display: inline-block;
+    max-width: 100px;
+    font-weight: 700;
+  }
+}
+
+</style>
