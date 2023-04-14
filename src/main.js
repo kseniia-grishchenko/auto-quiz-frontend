b8f105577c359c3ffce93fa89cfd60dea37cb577
kseniia <kseniia@smartiot.cloud>
2023-04-14 23:31:05 +0300

    correct file name

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import App from './App.vue';
 
-import './assets/main.css';
+import './assets/main.scss';
 
 Vue.use(ElementUI);
 
