f821cd11cf3f518955d74df68cae37ac1b84caa6
kseniia <kseniia@smartiot.cloud>
2023-04-15 22:53:35 +0300

    Registration View in progress

--- src/views/RegistrationView.vue
+++ src/views/RegistrationView.vue
@@ -1,34 +1,42 @@
 <template>
   <el-row class="content">
     <el-col :span="10">
-      <div class="input-section">
+      <el-form ref="form" :model="form" class="input-section" :rules="rules">
         <h3>Зареєструйтесь, щоб продовжити</h3>
-        <div>
+        <el-form-item prop="email">
           <span>Email</span>
-          <el-input placeholder="Введіть Email" v-model="email"></el-input>
-        </div>
-        <div>
+          <el-input placeholder="Введіть Email" v-model="form.email"></el-input>
+        </el-form-item>
+        <el-form-item prop="firstName">
           <span>Ім'я</span>
-          <el-input placeholder="Введіть імʼя" v-model="firstName"></el-input>
-        </div>
-        <div>
+          <el-input placeholder="Введіть імʼя" v-model="form.firstName"></el-input>
+        </el-form-item>
+        <el-form-item prop="lastName">
           <span>Прізвище</span>
-          <el-input placeholder="Введіть прізвище" v-model="lastName"></el-input>
-        </div>
-        <div>
+          <el-input placeholder="Введіть прізвище" v-model="form.lastName"></el-input>
+        </el-form-item>
+        <el-form-item prop="password">
           <span>Пароль</span>
-          <el-input placeholder="Введіть пароль" v-model="password" show-password></el-input>
-        </div>
-        <div>
+          <el-input placeholder="Введіть пароль" v-model="form.password" show-password></el-input>
+        </el-form-item>
+        <el-form-item>
           <span>Оберіть вашу роль</span>
           <div class="role-btns">
-            <el-button type="primary">Учень</el-button>
-            <el-button type="primary">Вчитель</el-button>
+            <el-button
+              :class="{'active': form.isTeacher === false}"
+              @click="form.isTeacher = false"
+            >Учень</el-button>
+            <el-button
+              :class="{'active': form.isTeacher === true}"
+              @click="form.isTeacher = true"
+            >Вчитель</el-button>
           </div>
-        </div>
-        <el-button type="primary" class="action">Зареєструватись</el-button>
-        <div>Вже зареєстровані? Увійти</div>
-      </div>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" class="action" @click="onSubmit">Зареєструватись</el-button>
+        </el-form-item>
+      </el-form>
+      <div>Вже зареєстровані? Увійти</div>
     </el-col>
     <el-col :span="12" :offset="2">
       <img src="/assets/icons/registration.png">
@@ -37,20 +45,64 @@
 </template>
 
 <script>
+import { postUnauthorized } from '../api.js';
+
 export default {
   data () {
     return {
-      email: '',
-      firstName: '',
-      lastName: '',
-      password: '',
-      role: ''
+      form: {
+        email: '',
+        firstName: '',
+        lastName: '',
+        password: '',
+        isTeacher: null
+      },
+      rules: {
+        email: [
+          { required: true, message: 'Це поле не може бути пустим', trigger: 'blur' },
+          { type: 'email', message: 'Email має бути валідним', trigger: ['blur', 'change'] }
+        ],
+        firstName: [
+          { required: true, message: 'Це поле не може бути пустим', trigger: 'blur' }
+        ],
+        lastName: [
+          { required: true, message: 'Це поле не може бути пустим', trigger: 'blur' }
+        ],
+        password: [
+          { required: true, message: 'Це поле не може бути пустим', trigger: 'blur' },
+          { min: 5, message: 'Мінімальна довжина паролю - 5 символів', trigger: 'change' }
+        ]
+      }
     };
+  },
+  methods: {
+    async onSubmit () {
+      try {
+        await postUnauthorized('/api/account/register/', {
+          email: this.form.email,
+          password: this.form.password,
+          first_name: this.form.firstName,
+          last_name: this.form.lastName,
+          is_teacher: this.form.isTeacher || false
+        });
+        this.$notify({
+          title: 'Успіх',
+          message: 'Акаунт успішно створено',
+          type: 'success'
+        });
+      } catch (err) {
+        this.$notify.error({
+          title: 'Помилка',
+          message: JSON.stringify(err.response.data)
+        });
+      }
+    }
   }
 };
 </script>
 
 <style lang="scss">
+@import '../assets/variables.scss';
 .action {
   span {
     font-weight: 700;
@@ -63,6 +115,13 @@ export default {
   span {
     font-size: 14px;
     line-height: 17px;
+  }
+
+  .active {
+    background-color: #CE3E7A;
+    span {
+      color: $main-bg;
+    }
   }
 }
 </style>
@@ -86,11 +145,11 @@ h3 {
 }
 
 .input-section {
-  div:not(:last-of-type) {
+  > div {
     margin-bottom: 24px;
   }
 
-  > div:last-of-type {
+  ~ div {
     text-align: center;
     margin-top: 24px
   }
