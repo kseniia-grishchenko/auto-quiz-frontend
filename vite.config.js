d4bfd735913d4e0eff8ba4e4e7a3f10ab961fd88
kseniia <kseniia@smartiot.cloud>
2023-04-15 22:53:08 +0300

    set up proxy

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,14 @@ import vue2 from '@vitejs/plugin-vue2'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:8000',
+        changeOrigin: true
+      }
+    }
+  },
   plugins: [
     vue2(),
     legacy({
