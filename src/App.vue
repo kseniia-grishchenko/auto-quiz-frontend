021ca73a4bd2be6af8b23baa225de967a82a0b56
kseniia <kseniia@smartiot.cloud>
2023-04-18 23:46:06 +0300

    handle token functionality in app root

--- src/App.vue
+++ src/App.vue
@@ -5,20 +5,94 @@
         <header-comp></header-comp>
       </el-header>
       <el-main>
-        <sign-up></sign-up>
+        <sign-up v-if="!user"></sign-up>
+        <sign-in v-if="!user" @log-in="handleLogIn"></sign-in>
       </el-main>
     </el-container>
   </div>
 </template>
 
 <script>
+import jwtDecode from 'jwt-decode';
+import { get, post } from './api.js';
+
 import HeaderComp from './comps/HeaderComp.vue';
 import SignUp from './views/SignUp.vue';
+import SignIn from './views/SignIn.vue';
 
 export default {
+  data: () => ({
+    expiresAt: null,
+    user: null,
+    type: 'password'
+  }),
+  methods: {
+    async logIn () {
+      const accessToken = localStorage.getItem('access');
+      if (!accessToken) return;
+
+      const { exp } = jwtDecode(accessToken);
+      this.expiresAt = exp;
+
+      if (this.expiresAt * 1e3 > Date.now()) {
+        await this.fetchUser();
+        return;
+      }
+
+      this.refreshToken();
+    },
+
+    async handleLogIn () {
+      await this.logIn();
+      location.hash = '#/';
+    },
+
+    logOut () {
+      localStorage.removeItem('access');
+      localStorage.removeItem('refresh');
+
+      this.user = null;
+    },
+
+    async fetchUser () {
+      try {
+        const { data: user } = await get('/api/account/me/');
+
+        this.user = user;
+      } catch (err) {
+        console.log(err);
+        console.error(err.response.data);
+      }
+    },
+
+    async refreshToken () {
+      try {
+        const { data } = await post('/api/account/token/refresh/', {
+          refresh: localStorage.getItem('refresh')
+        });
+
+        const { access, refresh } = data;
+
+        localStorage.setItem('access', access);
+        localStorage.setItem('refresh', refresh);
+        this.logIn();
+      } catch (err) {
+        console.error(err.response.data);
+      }
+    }
+  },
+  created () {
+    this.logIn();
+
+    setInterval(() => {
+      if (this.expiresAt && this.expiresAt * 1e3 > Date.now()) return;
+      this.refreshToken();
+    }, 60 * 1e3);
+  },
   components: {
+    HeaderComp,
     SignUp,
-    HeaderComp
+    SignIn
   }
 };
 </script>
