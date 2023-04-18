f52e28e5063a1ed869b4af2aa176ff1e15d540f8
kseniia <kseniia@smartiot.cloud>
2023-04-18 23:11:02 +0300

    import variables file

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue';
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
+import './assets/element-variables.scss';
+
 import App from './App.vue';
 
 import './assets/main.scss';
