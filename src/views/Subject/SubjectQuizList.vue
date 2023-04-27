c9be73bcd34893f95d592408871adb1cd3145dc6
kseniia <kseniia@smartiot.cloud>
2023-04-27 22:25:35 +0300

    SubjectQuizList component

---
+++ src/views/Subject/SubjectQuizList.vue
@@ -0,0 +1,28 @@
+<template>
+  <el-table
+  :data="quizzes"
+  :style="{
+    width: '100%',
+    fontSize: '20px',
+  }">
+  <el-table-column
+    prop="name"
+    label="Назва">
+  </el-table-column>
+</el-table>
+</template>
+
+<script>
+export default {
+  props: {
+    quizzes: {
+      type: Array,
+      default: () => []
+    }
+  }
+};
+</script>
+
+<style>
+
+</style>
