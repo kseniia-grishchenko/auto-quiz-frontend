170d387764833e215ab1d0c97dc05362a6b6a0aa
kseniia <kseniia@smartiot.cloud>
2023-04-27 23:13:41 +0300

    extract CreateBtn

---
+++ src/comps/CreateBtn.vue
@@ -0,0 +1,33 @@
+<template>
+  <el-button class="create-btn" @click="$emit('click')">
+    <span>Створити {{title}}</span>
+    <span>+</span>
+  </el-button>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      String,
+      default: ''
+    }
+  }
+};
+</script>
+
+<style>
+.create-btn > span {
+  display: flex;
+  justify-content: space-between;
+}
+
+.create-btn span {
+  font-size: 18px;
+  font-weight: 500;
+}
+
+.create-subject-btn span:last-of-type {
+  font-size: 22px;
+}
+</style>
