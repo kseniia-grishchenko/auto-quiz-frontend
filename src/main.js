2dfeecf26e2f7f90e236c05d61954900bc02c9fe
kseniia <kseniia@smartiot.cloud>
2023-05-05 00:30:11 +0300

    add ua language

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import ElementUI from 'element-ui';
+import locale from 'element-ui/lib/locale/lang/ua';
 import 'element-ui/lib/theme-chalk/index.css';
 import './assets/element-variables.scss';
 
@@ -7,7 +8,7 @@ import App from './App.vue';
 
 import './assets/main.scss';
 
-Vue.use(ElementUI);
+Vue.use(ElementUI, { locale });
 
 new Vue({
   render: (h) => h(App)
