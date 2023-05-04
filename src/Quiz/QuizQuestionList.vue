3dac637854fd79b53c3631843860b440b95c4d53
kseniia <kseniia@smartiot.cloud>
2023-05-04 14:45:46 +0300

    extract create question modal

--- src/Quiz/QuizQuestionList.vue
+++ src/Quiz/QuizQuestionList.vue
@@ -31,40 +31,14 @@
       В цьому квізі ще немає питань
     </div>
     <el-dialog :visible="createModalOpened" top="20vh" @close="$emit('close-create-modal')">
-      <el-form ref="form" :model="form" class="input-section">
-        <h3>Назва</h3>
-        <el-form-item>
-          <el-input
-            type="textarea"
-            :autosize="{ minRows: 3}"
-            placeholder="Введіть назву..."
-            maxlength="511"
-            v-model="form.title">
-          </el-input>
-        </el-form-item>
-        <h3>Очікувана відповідь</h3>
-        <el-form-item>
-          <el-input
-            type="textarea"
-            :autosize="{ minRows: 3}"
-            placeholder="Введіть відповідь..."
-            v-model="form.expectedAnswer">
-          </el-input>
-        </el-form-item>
-        <h3>Оцінка</h3>
-        <el-form-item>
-          <el-input-number v-model="form.value" :min="1"></el-input-number>
-        </el-form-item>
-        <el-form-item>
-        <el-button type="primary" class="action" @click="$emit('create-question', form)">Створити</el-button>
-        </el-form-item>
-      </el-form>
+      <create-question-modal v-on="$listeners"></create-question-modal>
     </el-dialog>
   </div>
 </template>
 
 <script>
 import CreateBtn from '../comps/CreateBtn.vue';
+import CreateQuestionModal from './CreateQuestionModal.vue';
 
 export default {
   props: {
@@ -83,20 +57,14 @@ export default {
       default: false
     }
   },
-  data: () => ({
-    form: {
-      title: '',
-      expectedAnswer: '',
-      value: 0
-    }
-  }),
   methods: {
     handleQuestionRedirect (question) {
       location.hash = `#/subjects/${this.subjectId}/quiz/${this.quizId}/question/${question.id}`;
     }
   },
   components: {
-    CreateBtn
+    CreateBtn,
+    CreateQuestionModal
   }
 };
 </script>
@@ -109,7 +77,7 @@ export default {
   width: 100%;
 }
 
-.no-quizzes {
+.no-questions {
   padding: 12px 0;
   font-size: 20px;
 }
